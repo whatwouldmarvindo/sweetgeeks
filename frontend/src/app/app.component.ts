871d8825c95790da316cd57f9508bf9c5487b12f
marvinbeckert <60096742+whatwouldmarvindo@users.noreply.github.com>
2021-09-28 20:11:50 +0200

    feat: css transition onSelect

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,27 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { startWith, map, debounceTime } from 'rxjs/operators';
+import { startWith, map } from 'rxjs/operators';
 import { Staedte } from './staedte';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('container') container: ElementRef | undefined
+  @ViewChild('solarFacts') solarFacts: ElementRef | undefined
   stadtInput = new FormControl()
   title = 'SweetGeeks'
   filteredStaedte: Observable<String[]> = this.stadtInput.valueChanges.pipe(
     startWith(''),
     map(stadt => stadt ? this._filter(stadt) : Staedte.slice())
   )
+  antwortIstDa = false
+  selectedStadt = ''
 
   private _filter(name: string): String[] {
     const filterValue = name.toLowerCase();
     return Staedte.filter(option => option.toLowerCase().includes(filterValue));
   }
 
-  submitting() {
-    console.log('HEYYY!!')
+  onSelectItem() {
+    this.selectedStadt = this.stadtInput.value
+    this.antwortIstDa = true
+    this.cssAnimation()
+  }
+
+  cssAnimation() {
+    const divEl: HTMLDivElement = this.container?.nativeElement
+    divEl.style.transition = '1s'
+    divEl.style.transform = 'translateY(-20vh)'
+    const ansEl: HTMLDivElement = this.solarFacts?.nativeElement
+    ansEl.style.opacity = '1'
+  }
+
+  reset() {
+    this.antwortIstDa = false
+    this.stadtInput.reset()
   }
 }
