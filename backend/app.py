439d8425379f3b8d2f469245a713713b66e47e13
Auron09 <=>
2021-09-28 21:42:04 +0200

    add test case

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import response_builder
+import json
 
 app = Flask(__name__)
 
@@ -8,6 +9,12 @@ def main():
     cityName = request.args.get("cityName")
     statistics = response_builder.buildStatistic(cityName)
     return jsonify(statistics)
+
+@app.route('/test', methods=['GET'])
+def test():
+    cityName = request.args.get("cityName")
+    statistics = [{'name' : "Grevenbroich", 'adresse' : "Breite Straße 1", 'modarea' : 'test', 'radabs' : 'test', 'kwh_kwp' : 'test', 'anzahl_0' : 'test', 'kw_17' : 'test', 'str_17' : "test"}]
+    return json.dumps(statistics)
 
 
 
