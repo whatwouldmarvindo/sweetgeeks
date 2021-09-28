26ea83a8a125fd15ee56fe03ea7b14506a73495e
Auron09 <=>
2021-09-28 21:12:11 +0200

    updated response_builder

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
-import communicator
+import response_builder
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def main():
     cityName = request.args.get("cityName")
-    statistics = communicator.buildStatistic(cityName)
+    statistics = response_builder.buildStatistic(cityName)
     return jsonify(statistics)
 
 
