6f5468b2e4cd64c776d567f5d72cfabbd05f698e
marvinbeckert <60096742+whatwouldmarvindo@users.noreply.github.com>
2021-09-29 11:43:45 +0200

    feat: liste moeglicher gebauede

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,27 +1,24 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from "@angular/material/button";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { HttpClientModule } from '@angular/common/http';
-import { registerLocaleData } from '@angular/common';
-import localeDe from '@angular/common/locales/de';
-import { Utf8Pipe } from './utf8.pipe';
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatButtonModule } from '@angular/material/button'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { HttpClientModule } from '@angular/common/http'
+import { registerLocaleData } from '@angular/common'
+import localeDe from '@angular/common/locales/de'
+import { Utf8Pipe } from './utf8.pipe'
+import { MatExpansionModule } from '@angular/material/expansion'
 
-registerLocaleData(localeDe);
-
+registerLocaleData(localeDe)
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Utf8Pipe
-  ],
+  declarations: [AppComponent, Utf8Pipe],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -31,9 +28,10 @@ registerLocaleData(localeDe);
     MatButtonModule,
     MatAutocompleteModule,
     MatSlideToggleModule,
-    HttpClientModule
+    HttpClientModule,
+    MatExpansionModule
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'de-DE'}],
-  bootstrap: [AppComponent]
+  providers: [{ provide: LOCALE_ID, useValue: 'de-DE' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
