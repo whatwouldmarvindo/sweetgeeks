9d7d1103b7aa08aaeecfe32745016b789d922076
Leokre <73553627+Leokre@users.noreply.github.com>
2021-10-28 19:54:58 +0200

    Moved Database credentials to environment

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import response_builder
 import json
 from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
 
@@ -18,11 +19,11 @@ def test():
     return json.dumps(statistics)
 
 
-app.config['MYSQL_USER']        = 'root'
-app.config['MYSQL_PASSWORD']    = 'toor'
-app.config['MYSQL_HOST']        = '0.0.0.0'
-app.config['MYSQL_DB']          = 'solarrechnerDB'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_USER']        = os.environ.get("MYSQL_USER")
+app.config['MYSQL_PASSWORD']    = os.environ.get("MYSQL_PASSWORD")
+app.config['MYSQL_HOST']        = os.environ.get("MYSQL_HOST")
+app.config['MYSQL_DB']          = os.environ.get("MYSQL_DB")
+#app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 db = MySQL(app)
 @app.route('/')
